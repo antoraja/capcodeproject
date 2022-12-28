2f9c671c7ce2c2f6a32de742b504482691e1d0fb
Antothomasraja Poobalarayan <antoraja1990de@gmail.com>
2022-12-28 17:06:52 +0000

    Last fix

---
+++ app/purchaseorderui/dist/Component-dbg.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("purchaseorderui.purchaseorderui.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
---
+++ app/purchaseorderui/dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("purchaseorderui.purchaseorderui.Component",{metadata:{manifest:"json"}})});
